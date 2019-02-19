e8cfb878944d6cc22d73e3922e3b2f6c8a013f24
DreamLuke <36737163+DreamLuke@users.noreply.github.com>
2019-02-19 14:45:00 +0200

    Create Cookie.php

---
+++ Cookie.php
@@ -0,0 +1,16 @@
+<?php 
+/**
+* 
+*/
+class Cookie
+{
+	
+	public function set(string $key, $values, int $limit = 0)
+	{
+		setcookie($key, $value, time()+$limit);
+	}
+	public function unset(string $key)
+	{
+		setcookie($key);
+	}
+}
